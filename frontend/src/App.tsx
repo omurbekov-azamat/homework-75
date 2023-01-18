a720054b44fd8e998887bcbb57945dcf0ddecafa
Omurbekov-Azamat <avgusttin8@gmail.com>
2023-01-18 18:32:51 +0600

    installed axios and @mui and @emotion; added some @mui components;

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import {Button, Container, Grid, TextField} from "@mui/material";
+
 function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+    return (
+        <Container maxWidth='xl'>
+            <Grid container direction="column" spacing={2}>
+                <Grid item xs>
+                    <TextField id="encode" label="Encode" name="encode" sx={{mr: 2}}/>
+                    <Button type="button" color="primary" variant="contained">down</Button>
+                </Grid>
+                <Grid item xs>
+                    <TextField id="password" label="Password" name="password"/>
+                </Grid>
+                <Grid item xs>
+                    <TextField id="decode" label="Decode" name="decode" sx={{mr: 2}}/>
+                    <Button type="button" color="secondary" variant="contained">up</Button>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
 export default App;
